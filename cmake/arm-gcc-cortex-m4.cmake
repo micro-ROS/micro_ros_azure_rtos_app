d14d5bdcae41f7a2ab234e0bac57f36b009c7557
Pablo Garrido <pablogs9@gmail.com>
2024-06-03 11:07:40 +0200

    Bump jazzy (#16)
    * Bump jazzy
    
    * Bump ubuntu
    
    * Fix pip
    
    * Bump flags

--- cmake/arm-gcc-cortex-m4.cmake
+++ cmake/arm-gcc-cortex-m4.cmake
@@ -5,7 +5,7 @@
 set(THREADX_ARCH "cortex_m4")
 set(THREADX_TOOLCHAIN "gnu")
 
-set(MCPU_FLAGS "-mthumb -mcpu=cortex-m4")
+set(MCPU_FLAGS "-mthumb -mcpu=cortex-m4 -Wno-address")
 set(VFP_FLAGS "-mfloat-abi=hard -mfpu=fpv4-sp-d16")
 
 include(${CMAKE_CURRENT_LIST_DIR}/arm-gcc-cortex-toolchain.cmake)
